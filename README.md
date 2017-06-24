4cca9b3eb51361e6a4f68e9cc2ff61952701ca49
Russell Leake <gt3389b@gmail.com>
2017-06-24 12:47:02 -0400

    Actually deploy to AWS and add README

---
+++ README.md
@@ -0,0 +1,16 @@
+# UUID Lambda
+
+A lambda function which generates a UUID
+
+## Install
+
+Install the local node_modules required to run in Lambda
+
+``` ./install.sh ```
+
+## Deploy to AWS Lambda
+
+Deploys to a function named 'uuid'
+
+``` ./deploy.sh ```
+
--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,7 @@ cd $(dirname $0)
 pushd ${PROJECT}
 zip -r ../${PROJECT}.zip .
 popd
+
+#aws lambda create-function --function-name uuid --runtime "nodejs6.10" --role "arn:aws:iam::710516728004:role/lambda_basic_execution" --handler "lambda_function.handler" --zip-file fileb://uuid-generator.zip
+
+aws lambda update-function-code --function-name "uuid" --zip-file fileb://uuid-generator.zip
